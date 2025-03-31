e5c2f3ad1630af1829b8c2c02b38afc28df781ce
Adrian Porlan <adriporlanlopez11@gmail.com>
2025-03-31 15:10:13 +0200

    refactor: update Astro configuration and remove unused data collector script

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
-
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'server',
-  adapter: cloudflare()
+  
+ 
 });
