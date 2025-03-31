93a6b983b1c5be9336db93dcd322abba8c6e4416
Adrian Porlan <adriporlanlopez11@gmail.com>
2025-03-31 15:16:55 +0200

    change to static

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
   
   integrations: [react()],
 
